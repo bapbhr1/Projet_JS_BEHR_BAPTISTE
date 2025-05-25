5837637a73310fdd0ef25769005c65782db5a267
bapbhr1 <baptiste.behr@gmail.com>
2025-05-25 23:23:44 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,1 @@
-const gulp = require('gulp');
-const minify = require('gulp-minify');
 
-gulp.task('minify-js', () => {
-    return gulp.src('index.js') // ou le fichier à minifier
-        .pipe(minify())
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('default', gulp.series('minify-js'));
